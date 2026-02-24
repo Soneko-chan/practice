6082c80a40712c9107985cc766a2e9b6ff71004f
bochkarevsk <pimypy2021@gmail.com>
2026-02-24 10:24:06 +0700

    TAsk1_part1

--- main/build.gradle.kts
+++ main/build.gradle.kts
@@ -37,8 +37,17 @@ android {
 
 dependencies {
 
-    implementation("com.android.support:appcompat-v7:28.0.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+    implementation("com.google.android.material:material:1.12.0") // Updated to latest stable
+
+
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.10.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.10.0")
+    implementation("androidx.fragment:fragment-ktx:1.8.9")
+
+
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("com.android.support.test:runner:1.0.2")
-    androidTestImplementation("com.android.support.test.espresso:espresso-core:3.0.2")
+    androidTestImplementation("androidx.test:runner:1.6.2") // Updated to AndroidX version
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1") // Updated to AndroidX version
 }
