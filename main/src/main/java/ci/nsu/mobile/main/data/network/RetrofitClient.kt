f8538b4d45fb0ea6e23a58bd177a17cf6b45c2ae
Soneko <Soneko@DESKTOP-73OJCBS>
2026-05-04 21:55:50 +0700

    first commit- programm connect to server

--- main/src/main/java/ci/nsu/mobile/main/data/network/RetrofitClient.kt
+++ main/src/main/java/ci/nsu/mobile/main/data/network/RetrofitClient.kt
@@ -11,6 +11,7 @@ import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
     private const val BASE_URL = "http://10.0.2.2:8080/api/"
+    //private const  val baseUrl: String = "http://192.168.200.160:8080/api/" //использовать в колледже
 
     private val json = Json { ignoreUnknownKeys = true }
     private val contentType = "application/json".toMediaType()
